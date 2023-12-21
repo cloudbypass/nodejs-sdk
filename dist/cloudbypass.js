9027273564671c494461f090b713d3173d135638
ChengHoward <18071131140teltphone@gmail.com>
2023-12-21 17:57:52 +0800

    chore(release): v0.0.1

--- dist/cloudbypass.js
+++ dist/cloudbypass.js
@@ -1,4 +1,4 @@
-// Cloudbypass v0.0.1-alpha.5 Copyright (c) 2023 NULL and contributors
+// Cloudbypass v0.0.1 Copyright (c) 2023 NULL and contributors
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('punycode'), require('util'), require('url')) :
   typeof define === 'function' && define.amd ? define(['punycode', 'util', 'url'], factory) :
